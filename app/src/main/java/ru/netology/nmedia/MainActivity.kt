d17aa8acfeeccb6864e9e6f764cb39524f8fb6b2
Ivan Dashkov <iadashkov@gmail.com>
2024-02-02 08:05:47 +0900

    Task 1.4

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.likes.setOnClickListener {
+            binding.likes.setImageResource(R.drawable.ic_baseline_favorite_24)
+        }
     }
+
 }
