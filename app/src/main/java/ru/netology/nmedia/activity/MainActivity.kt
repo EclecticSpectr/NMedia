4eef9690768e6035e27d4e01ee94959b2717d198
Ivan Dashkov <iadashkov@gmail.com>
2024-04-06 07:36:07 +0900

    restored

---
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -0,0 +1,87 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostsAdapter
+import ru.netology.nmedia.adapter.onListener
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.util.AndroidUtils
+import ru.netology.nmedia.util.AndroidUtils.focusAndShowKeyboard
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: PostViewModel by viewModels()
+        val adapter = PostsAdapter(object : onListener {
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                viewModel.shareById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+        })
+
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            val newPost = adapter.currentList.size < posts.size && adapter.currentList.size > 0
+            adapter.submitList(posts) {
+                if (newPost) {
+                    binding.list.smoothScrollToPosition(0)
+                }
+            }
+        }
+
+        viewModel.edited.observe(this) { post ->
+            with(binding.edit) {
+                if (post.id != 0L) {
+                    binding.bannerGroup.visibility = View.VISIBLE
+                    setText(post.content)
+                    focusAndShowKeyboard()
+                }
+            }
+        }
+
+        binding.closeButton.setOnClickListener {
+            viewModel.clear()
+            binding.edit.setText("")
+            binding.edit.clearFocus()
+            AndroidUtils.hideKeyboard(it)
+            binding.bannerGroup.visibility = View.GONE
+            return@setOnClickListener
+        }
+
+        binding.save.setOnClickListener {
+            val text = binding.edit.text.toString().trim()
+            if (text.isEmpty()) {
+                Toast.makeText(this, R.string.error_empty_content, Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            viewModel.changeContentAndSave(text)
+            binding.edit.setText("")
+            binding.edit.clearFocus()
+            AndroidUtils.hideKeyboard(it)
+
+            binding.bannerGroup.visibility = View.GONE
+        }
+    }
+}
---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -0,0 +1,89 @@
+package ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+import kotlin.math.floor
+
+interface onListener {
+    fun onLike(post: Post)
+    fun onShare(post: Post)
+    fun onRemove(post: Post)
+    fun onEdit(post: Post)
+}
+
+class PostsAdapter(private val listener: onListener) :
+    ListAdapter<Post, PostViewHolder>(PostDiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, listener)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class PostViewHolder(private val binding: CardPostBinding, private val listener: onListener) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(post: Post) {
+        with(binding) {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            likesButton.isChecked = post.likedByMe
+            likesButton.text = amountShow(post.likes)
+            sharesButton.text = amountShow(post.shares)
+            viewsButton.text = post.views.toString()
+            likesButton.setOnClickListener {
+                listener.onLike(post)
+            }
+            sharesButton.setOnClickListener {
+                listener.onShare(post)
+            }
+            menuButton.setOnClickListener {
+                PopupMenu(it.context, it).apply {
+                    inflate(R.menu.options_post)
+                    setOnMenuItemClickListener { item ->
+                        when (item.itemId) {
+                            R.id.remove -> {
+                                listener.onRemove(post)
+                                true
+                            }
+
+                            R.id.edit -> {
+                                listener.onEdit(post)
+                                true
+                            }
+
+                            else -> false
+                        }
+                    }
+                }.show()
+            }
+        }
+    }
+}
+
+private fun amountShow(number: Int): String =
+    when {
+        number >= 1_000_000 && (number / 100_000) % 10 == 0 -> (number / 1_000_000).toString() + "M"
+        number >= 1_000_000 -> (floor(number.toDouble() / 100_000) / 10).toString() + "M"
+        number >= 10_000 || (number >= 1_000 && (number / 100) % 10 == 0) -> (number / 1_000).toString() + "K"
+        number >= 1_000 -> (floor(number.toDouble() / 100) / 10).toString() + "K"
+        else -> number.toString()
+    }
+
+object PostDiffCallback : DiffUtil.ItemCallback<Post>() {
+    override fun areItemsTheSame(oldItem: Post, newItem: Post) = oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: Post, newItem: Post) = oldItem == newItem
+
+}
